707c0d8c656c2e3fd00b1391d058d4c21debbeeb
rphmeier <rphmeier@gmail.com>
2015-11-01 19:14:06 -0500

    Moved to Key trait based version

--- src/lib.rs
+++ src/lib.rs
@@ -5,193 +5,77 @@
 use std::any::{Any, TypeId};
 use std::mem;
 
+pub trait Key {
+    type Value: Any;
+}
+
 /// The building block trait for recursive variadics.
 pub trait RecursiveVariadic {
-    /// Try to get the field of type N.
-    fn get<N: Any>(&self) -> Option<&N>;
-    /// Try to get the field of type N mutably.
-    fn get_mut<N: Any>(&mut self) -> Option<&mut N>;
-    /// Add a field of type N to this.
-    fn and<N: Any>(self, val: N) -> Entry<N, Self> where Self: Sized {
+    /// Try to get the value for N.
+    fn get<N: Key>(&self) -> Option<&N::Value>;
+    /// Try to get the value for N mutably.
+    fn get_mut<N: Key>(&mut self) -> Option<&mut N::Value>;
+    /// Add a key-value pair to this.
+    fn and<N: Key>(self, val: N::Value) -> Entry<N, Self> where Self: Sized {
         Entry {
             data: val,
             parent: self,
         }
+    }
+    /// Add the default value for N
+    fn and_default<N: Key>(self) -> Entry<N, Self> 
+    where N::Value: Default, Self: Sized {
+        self.and(N::Value::default())
     }
 }
 
 /// The base case for recursive variadics: no fields.
 pub type Empty = ();
 impl RecursiveVariadic for Empty {
-    fn get<N: Any>(&self) -> Option<&N> { None }
-    fn get_mut<N: Any>(&mut self) -> Option<&mut N> { None }
+    fn get<N: Key>(&self) -> Option<&N::Value> { None }
+    fn get_mut<N: Key>(&mut self) -> Option<&mut N::Value> { None }
 }
 
 /// Wraps some field data and a parent, which is either another Entry or Empty
-pub struct Entry<T, R> {
-    data: T,
+pub struct Entry<T: Key, R> {
+    data: T::Value,
     parent: R,
 }
 
-impl<T: Any, R: RecursiveVariadic> RecursiveVariadic for Entry<T, R> {
-    fn get<N: Any>(&self) -> Option<&N> { 
-        if TypeId::of::<N>() == TypeId::of::<T>() {
+impl<T: Key, R: RecursiveVariadic> RecursiveVariadic for Entry<T, R> {
+    fn get<N: Key>(&self) -> Option<&N::Value> { 
+        if TypeId::of::<N::Value>() == TypeId::of::<T::Value>() {
             Some(unsafe { mem::transmute(&self.data) })
         } else {
-            self.parent.get()
+            self.parent.get::<N>()
         }
     }
-    fn get_mut<N: Any>(&mut self) -> Option<&mut N> { 
-        if TypeId::of::<N>() == TypeId::of::<T>() {
+    fn get_mut<N: Key>(&mut self) -> Option<&mut N::Value> { 
+        if TypeId::of::<N::Value>() == TypeId::of::<T::Value>() {
             Some(unsafe { mem::transmute(&mut self.data) })
         } else {
-            self.parent.get_mut()
+            self.parent.get_mut::<N>()
         }
     }
-}
-
-/// This macro is a standin for the way this would be implemented 
-/// with Higher Kinded Types. It creates a wrapper struct that for every N
-/// puts an entry in the RecursiveVariadic it wraps for a Output<N>.
-///
-/// # Examples
-///
-/// This example creates a wrapper that gets a thing.
-///
-/// ```rust
-///     #[macro_use]
-///     extern crate recursive_variadic;
-///
-///     use recursive_variadic::*;
-///
-///     create_wrapper!(VecHolder, N => Vec<N>);
-/// 
-///     fn vec_variadic() {
-///         // this struct maps types to a vector of that type.
-///         let mut my_vecs = VecHolder::new()
-///             .and_default::<usize>()
-///             .and_default::<bool>()
-///             .and::<u32>(Vec::new()); // if you want to provide a value
-///         my_vecs.get_mut::<usize>().unwrap().push(2);
-///         my_vecs.get_mut::<bool>().unwrap().push(false);
-///         my_vecs.get_mut::<u32>().unwrap().push(0);
-/// 
-///         assert!(my_vecs.get::<String>().is_none());
-///     }
-///
-///     fn main() { vec_variadic() }
-/// ```
-///
-/// Using an creating a wrapper around an Id type is redundant,
-/// but better shows what this macro does.
-///
-/// ```rust
-///     #[macro_use]
-///     extern crate recursive_variadic;
-///
-///     use recursive_variadic::*;
-///
-///     type Id<N> = N;
-/// 
-///     fn id_variadic() {
-///         create_wrapper!(IdVariadic, N => Id<N>, PRIV);
-///         let mut my_things = IdVariadic::new()
-///             .and_default::<usize>()
-///             .and(23i32);
-/// 
-///         *my_things.get_mut::<i32>().unwrap() += 1;
-///         assert!(my_things.get::<usize>().is_some());
-///     }
-///     
-///     fn main() { id_variadic() }
-/// ```
-
-#[macro_export]
-macro_rules! create_wrapper {
-    ($name:ident, N => $output:ident<N>) => {
-        create_wrapper!($name, $output, pub);
-    };
-
-    ($name:ident, N => $output:ident<N>, PRIV) => {
-        create_wrapper!($name, $output, );
-    };
-
-    ($name:ident, $output:ident, $($vis:tt)*) => {
-        use std::any::Any;
-        create_wrapper!(
-            $($vis)* struct $name<R> {
-                data: R,
-            }
-        );
-
-        create_wrapper!(
-            impl $name<()> {
-                $($vis)* fn new() -> Self {
-                    $name {
-                        data: ()
-                    }
-                }
-            }
-        );
-
-        create_wrapper!(
-            impl<R: RecursiveVariadic> $name<R> {
-                $($vis)* fn get<N>(&self) -> Option<&$output<N>>
-                where $output<N>: Any {
-                    self.data.get()
-                }
-
-                $($vis)* fn get_mut<N>(&mut self) -> Option<&mut $output<N>>
-                where $output<N>: Any {
-                    self.data.get_mut()
-                }
-
-                $($vis)* fn and<N: Any>(self, val: $output<N>) -> $name<Entry<$output<N>, R>>
-                where $output<N>: Any {
-                    $name {
-                        data: self.data.and(val)
-                    }
-                }
-
-                $($vis)* fn and_default<N: Any>(self) -> $name<Entry<$output<N>, R>>
-                where $output<N>: Any + Default {
-                    $name {
-                        data: self.data.and(Default::default())
-                    }
-                }
-            }
-        );
-    };
-
-    ($i:item) => {$i};
 }
 
 #[cfg(test)]
 mod tests {
+    use std::any::Any;
+
     use super::*;
 
     #[test]
     fn it_works() {
-        let thing = ().and(23).and(0usize).and("Hello!");
+        impl Key for i32 { type Value = i32; }
+        impl Key for usize { type Value = Vec<usize>; }
+        impl Key for bool { type Value = bool; }
+        impl Key for &'static str { type Value = &'static str; }
+
+        let mut thing = ().and::<i32>(23).and_default::<usize>().and::<&'static str>("Hello!");
+        thing.get_mut::<usize>().unwrap().push(1);
         assert!(thing.get::<i32>().is_some());
-        assert!(thing.get::<usize>().is_some());
         assert!(thing.get::<&'static str>().is_some());
         assert!(thing.get::<bool>().is_none());
-    }
-
-    create_wrapper!(VecHolder, N => Vec<N>);
-
-    #[test]
-    fn vec_variadic() {
-        // this struct will hold a vector of usize, bool, and u32.
-        let mut my_vecs = VecHolder::new()
-            .and_default::<usize>()
-            .and_default::<bool>()
-            .and::<u32>(Vec::new()); // just to prove they're the same.
-        my_vecs.get_mut::<usize>().unwrap().push(2);
-        my_vecs.get_mut::<bool>().unwrap().push(false);
-        my_vecs.get_mut::<u32>().unwrap().push(0);
-
-        assert!(my_vecs.get::<String>().is_none());
     }
 }
